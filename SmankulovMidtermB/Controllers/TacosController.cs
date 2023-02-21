0696707c40537514f27f96330d8b54faa3e0885c
Anatay <smanfulhasry776@gmail.com>
2023-02-21 17:57:34 -0500

    All

--- SmankulovMidtermB/Controllers/TacosController.cs
+++ SmankulovMidtermB/Controllers/TacosController.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.Xml.Schema;
+using MessagePack;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
@@ -13,6 +15,7 @@ namespace SmankulovMidtermB.Controllers
     public class TacosController : Controller
     {
         private readonly ApplicationDbContext _context;
+        private string Size;
 
         public TacosController(ApplicationDbContext context)
         {
@@ -22,7 +25,7 @@ namespace SmankulovMidtermB.Controllers
         // GET: Tacos
         public async Task<IActionResult> Index()
         {
-              return View(await _context.Taco.ToListAsync());
+              return View(await _context.Taco.OrderByDescending(DateTime => DateTimeKind).ToList());
         }
 
         // GET: Tacos/Details/5
@@ -54,14 +57,46 @@ namespace SmankulovMidtermB.Controllers
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("Id,Size,Filling,FirstName,LastName,Phone,DateRequested,Total")] Taco taco)
+        public async Task<IActionResult> Create([Bind("Id,Size,Filling,FirstName,LastName,Phone,DateRequested")] Taco taco)
         {
+            double total1 = 0;
+
             if (ModelState.IsValid)
             {
+                if (taco.Size == "Small")
+                {
+                    total1 += 8;
+                }
+                else if (taco.Size == "Medium")
+                {
+                    total1 += 10;
+                }
+                else if (taco.Size == "Large")
+                {
+                    total1 += 12;
+                }
+
+                double total2 = 0;
+
+                 if (taco.Filling == "Beans")
+                {
+                    total2 += 6;
+                }
+                else if (taco.Filling == "Fish")
+                {
+                    total2 += 8;
+                }
+                else if (taco.Filling == "Shrimp")
+                {
+                    total2 += 10;
+                }
+                taco.Total = total1 + total2;
                 _context.Add(taco);
                 await _context.SaveChangesAsync();
                 return RedirectToAction(nameof(Index));
             }
+            
+
             return View(taco);
         }
 
--- SmankulovMidtermB/Models/Taco.cs
+++ SmankulovMidtermB/Models/Taco.cs
@@ -1,20 +1,25 @@
-﻿namespace SmankulovMidtermB.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SmankulovMidtermB.Models
 {
     public class Taco
     {
        public int Id { get; set; }
+        [Display(Name = "make your taco bigger)")]
+        public string? Size { get; set; }
+        [Display(Name = "make your taco tastier)")]
+        public string? Filling { get; set; }
 
-       public string? Size { get; set; }
-       public string? Filling { get; set; }
-
+        [MinLength(3)]
        public string? FirstName { get; set; }
+        [Required]
        public string? LastName { get; set; }
-       public string? Phone { get; set; }
-       public DateTime DateRequested { get; set; }
+        [Required]
+        public string? Phone { get; set; }
+        [Display(Name = "day month year")]
+        public DateTime DateRequested { get; set; }
+        public double Total { get; set; }
 
-       public double Total { get; set; }
-
-
-
+     
     }
 }
